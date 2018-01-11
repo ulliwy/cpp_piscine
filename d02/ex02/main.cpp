0907ad6bc316e6d5a637371591dad802373c05ed
ulliwy <inessa.t@icloud.com>
2018-01-10 22:36:01 -0800

    d02: ex02-ex05 done.

--- d02/ex02/main.cpp
+++ d02/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iprokofy <iprokofy@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Ulliwy <Ulliwy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/10 15:06:19 by iprokofy          #+#    #+#             */
-/*   Updated: 2018/01/10 16:48:04 by iprokofy         ###   ########.fr       */
+/*   Updated: 2018/01/10 20:07:39 by Ulliwy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,45 @@
 
 #include <iostream>
 int main( void ) {
-	Fixed a(300);
-	Fixed b(30);
+	// Fixed a(0);
+	// Fixed b(-1);
 
-	std::cout << (a + b) << std::endl;
-	std::cout << a - b << std::endl;
-	std::cout << a * b << std::endl;
-	std::cout << a / b << std::endl;
-	// Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-
+	// std::cout << a + b << std::endl;
+	// std::cout << a - b << std::endl;
+	// std::cout << a * b << std::endl;
+	// std::cout << a / b << std::endl;
+	
+	// std::cout << "-------" << std::endl;
+	// std::cout << (a > b) << std::endl;
+	// std::cout << (a >= b) << std::endl;
+	// std::cout << (a < b) << std::endl;
+	// std::cout << (a <= b) << std::endl;
+	// std::cout << (a == b) << std::endl;
+	// std::cout << (a != b) << std::endl;
+	
+	// std::cout << "-------" << std::endl;
+	// std::cout << a++ << std::endl;
 	// std::cout << a << std::endl;
 	// std::cout << ++a << std::endl;
 	// std::cout << a << std::endl;
-	// std::cout << a++ << std::endl;
-	// std::cout << a << std::endl;
+	// std::cout << b-- << std::endl;
 	// std::cout << b << std::endl;
-	// std::cout << Fixed::max( a, b ) << std::endl;
+	// std::cout << --b << std::endl;
+	// std::cout << b << std::endl;
+
+	// std::cout << "-------" << std::endl;
+	// std::cout << min(a, b) << std::endl;
+	// std::cout << max(a, b) << std::endl;
+
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
 	return 0;
+
 }
